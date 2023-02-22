9d8bb9d4fe4242824ae8779df52652f91ca398ee
James <jd.bannister28@gmail.com>
2023-02-22 13:34:26 +0000

    zoom window finished - needs polishing

--- test pages/image_page.js
+++ test pages/image_page.js
@@ -1,20 +1,39 @@
 const image = document.querySelector("#image");
-const body = document.querySelector("body");
-const frame = document.querySelector('#img-window');
-const slider = document.querySelector("#zoom-slider");
-const output = document.querySelector("#test");
+const frame = document.querySelector('#frame');
+const slider = document.querySelector("#slider");
+const fullScreenBtn = document.querySelector("#full-screen");
 
-var scrollAmount;
-let mouseDown = false;
-let startX, scrollLeft;
-let startY, scrollTop;
-let mouseX, mouseY;
-let zoomed = false;
-let fitHeight = image.height;
-let fitWidth = image.width;
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
 
-function fullScreen () {
-  const elem = document.querySelector("#img-window");
+
+let zoomValue;
+let isMouseDown = false;
+let mousedownX;
+let mousedownY;
+let mouseX;
+let mouseY;
+let scrollAmountX;
+let scrollAmountY;
+frame.scrollBy({behavior: 'smooth'});
+
+let fitWindow = function(){
+  if ((image.naturalHeight/frame.offsetHeight) > (image.naturalWidth/frame.offsetWidth)) {
+    image.height = frame.offsetHeight;
+    image.width = image.naturalWidth * (image.height / image.naturalHeight);
+  }
+  else {
+    image.width = frame.offsetWidth;
+    image.height = image.naturalHeight * (image.width / image.naturalWidth);
+  }
+  zoomValue = image.height/image.naturalHeight;
+  slider.min = zoomValue;
+  slider.value = zoomValue;
+}
+
+let fullScreen = function() {
+  const elem = document.querySelector("#frame");
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
   } else if (elem.webkitRequestFullscreen) { /* Safari */
@@ -22,103 +41,96 @@ function fullScreen () {
   } else if (elem.msRequestFullscreen) { /* IE11 */
     elem.msRequestFullscreen();
   }
-};
+}
 
-let zoom = function() {
-  image.style.maxWidth = "none";
-  image.style.height = "auto";
-  image.height = fitHeight + (slider.value/100) * (image.naturalHeight - fitHeight);
-  image.width = fitWidth + (slider.value/100) * (image.naturalWidth - fitWidth);
+let setZoom = function(zoomTo) {
+  imageCenterX = (frame.offsetWidth / 2 + frame.scrollLeft) / zoomValue;
+  imageCenterY = (frame.offsetHeight / 2 + frame.scrollTop) / zoomValue;
+  if (zoomTo > 1) {zoomValue = 1;}
+  else if (zoomTo < slider.min) {zoomValue = slider.min;}
+  else {zoomValue = zoomTo;}
+  image.height = image.naturalHeight * zoomValue;
+  image.width = image.naturalWidth * zoomValue;
+  slider.value = zoomValue;
+  frame.scrollLeft = imageCenterX * zoomValue - frame.offsetWidth / 2
+  frame.scrollTop = imageCenterY * zoomValue - frame.offsetHeight / 2
 }
 
 let keyMove = function(e) {
-  scrollAmount = 10;
-  frame.scrollBy({behavior: 'smooth'});
+  let scrollAmount = 10 * zoomValue;
   switch (e.key){
     case "ArrowLeft":
-      frame.scrollBy({left: -scrollAmount,});
+      scrollAmountX = -scrollAmount;
       break;
     case "ArrowRight":
-      frame.scrollBy({left: +scrollAmount,});
+      scrollAmountX = +scrollAmount;
       break;
     case "ArrowUp":
-      frame.scrollBy({top: -scrollAmount,});
+      scrollAmountY = -scrollAmount;
       break;
     case "ArrowDown":
-      frame.scrollBy({top: +scrollAmount,});
+      scrollAmountY = +scrollAmount;
       break;
   }
-};
-
-let keyStop = function(e) {
-  scrollAmount = 0;
-};
-
-let zoomIn = function (e) {
-  let scrToX, scrToY;
-  scrToX = (e.pageX - image.offsetLeft + frame.scrollLeft) * image.naturalWidth / image.width - (0.5 * frame.offsetWidth);
-  scrToY = (e.pageY - image.offsetTop + frame.scrollTop) * image.naturalHeight / image.height - (0.5 * frame.offsetHeight);
-  if (scrToX < 0){scrToX = 0};
-  if (scrToY < 0){scrToY = 0};
-  image.style.height = "2480px";
-  image.style.cursor = "grab";
-  image.style.maxWidth = "none";
-  frame.scrollLeft = scrToX;
-  frame.scrollTop = scrToY;
-  zoomed = true;
 }
 
-let zoomOut = function (e) {
-  image.style.height = "inherit";
-  image.style.cursor = "zoom-in";
-  image.style.maxWidth = "100%";
-  zoomed = false;
-}
+fitWindow();
 
-let startDragging = function (e) {
-  mouseDown = true;
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-  startX = e.pageX - frame.offsetLeft;
-  scrollLeft = frame.scrollLeft;
-  startY = e.pageY - frame.offsetTop;
-  scrollTop = frame.scrollTop;
-  image.style.cursor = "grabbing";
-};
+setInterval (function() {
+  frame.scrollBy({
+    top: scrollAmountY,
+    left: scrollAmountX
+  });
+},10);
 
-let stopDragging = function (e) {
-  mouseDown = false;
-  image.style.cursor = "grab";
-  if (e.pageX == mouseX && e.pageY == mouseY) {
-    if (zoomed == false) {zoomIn(e);}
-    else if (zoomed == true) {zoomOut(e)};
-    
-  };
-};
+window.addEventListener("resize", fitWindow);
 
-frame.addEventListener('mousemove', (e) => {
-  e.preventDefault();
-  if(!mouseDown) { return; }
-  const x = e.pageX - frame.offsetLeft;
-  const scrollX = x - startX;
-  frame.scrollLeft = scrollLeft - scrollX;
-
-  const y = e.pageY - frame.offsetTop;
-  const scrollY = y - startY;
-  frame.scrollTop = scrollTop - scrollY;
+document.addEventListener('keydown', keyMove);
+document.addEventListener('keyup', () => {
+  scrollAmountX = 0;
+  scrollAmountY = 0;
 });
 
-frame.addEventListener('wheel', (e) => {
+fullScreenBtn.addEventListener('click', fullScreen);
+
+image.addEventListener('mousedown', (e) => {
+  isMouseDown = true;
+  mousedownX = e.pageX;
+  mousedownY = e.pageY;
+  mouseX = mousedownX;
+  mouseY = mousedownY;
+});
+
+image.addEventListener('mousemove', (e) => {
   e.preventDefault();
+  if(!isMouseDown) {return;}
+  frame.scrollLeft += mouseX - e.pageX;
+  frame.scrollTop += mouseY - e.pageY;
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+  image.style.cursor = "grabbing";
 })
 
-// Add the event listeners
-frame.addEventListener('mousedown', startDragging);
-frame.addEventListener('mouseup', stopDragging);
-frame.addEventListener('mouseleave', stopDragging);
-frame.addEventListener('touchstart', startDragging);
-frame.addEventListener('touchend', stopDragging);
-document.addEventListener('keydown', keyMove);
-document.addEventListener('keyup', keyStop);
-slider.addEventListener('input', zoom);
+image.addEventListener('mouseup', (e) => {
+  isMouseDown = false;
+  /* check if image dragged and zoom if not dragged */
+  if ((e.pageX == mousedownX && e.pageY == mousedownY)) {
+    setZoom(zoomValue * 1.2)
+  }
+  image.style.cursor = "zoom-in";
+});
 
+image.addEventListener('mouseleave', () => {
+  isMouseDown = false;
+  image.style.cursor = "zoom-in";
+});
+
+image.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  setZoom(zoomValue - 0.001*e.deltaY)
+})
+
+slider.addEventListener('input', (e) => {
+  setZoom(e.target.value)
+  slider.blur();
+});
